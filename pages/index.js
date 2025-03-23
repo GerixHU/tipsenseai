60f341db116a56cd21da6a51ff3f015cecb76019
GerixHU <gergo2001725@gmail.com>
2025-03-23 12:42:03 +0100

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+export default function Home() {
+  return (
+    <div style={{
+      backgroundColor: '#0f0f0f',
+      color: '#fff',
+      height: '100vh',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center'
+    }}>
+      <h1 style={{ fontSize: '3rem' }}>👋 Üdv a TipSenseAI-n!</h1>
+      <p style={{ marginTop: '1rem' }}>Ez a te saját sportfogadási AI rendszered alapja.</p>
+    </div>
+  );
+}
